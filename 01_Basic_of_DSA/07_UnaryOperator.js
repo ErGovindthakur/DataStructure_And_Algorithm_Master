c00e6056528f49c09655e5817f2ac0f9ffe0338e
ErGovindthakur <ergovindthakur@gmail.com>
2025-05-12 12:56:42 +0530

    just exploring math function in js

---
+++ 01_Basic_of_DSA/07_UnaryOperator.js
@@ -0,0 +1,20 @@
+// Exploring unary operator in js
+
+let num = 11;
+
+let unarySum = num++ + ++num; // 11 + 13 = 24
+console.log(unarySum);
+
+let a = 11; 
+let b = 22;
+let c = a + b + a++ + b++ + ++a + ++b; 
+// c = 11 + 22 + 11 + 22 + 13 + 24 = 103
+console.log(c);
+
+
+let B = true;
+B++;
+console.log(B);
+
+// Note => Can't apply unary operator at constant
+// eg -: 1++ (not correct)
