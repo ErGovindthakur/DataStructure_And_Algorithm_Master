1a3110b3146d309b9f320ab8052ada3d9150ae43
ErGovindthakur <ergovindthakur@gmail.com>
2025-05-12 12:00:17 +0530

    solve sum of digit problem

---
+++ 01_Basic_of_DSA/04_SumOfDigit.js
@@ -0,0 +1,10 @@
+// Find the sum of digits
+
+let myDigit = 99999;
+let sumOfDigit = 0;
+while(myDigit>0){
+     let rem = myDigit%10;
+     sumOfDigit += rem;
+     myDigit = Math.floor(myDigit/10);
+}
+console.log("sumOfDigit ", sumOfDigit);
