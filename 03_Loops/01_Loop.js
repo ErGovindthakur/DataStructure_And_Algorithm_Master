6c345dd07b760fb489361eba4207e490699c52c0
ErGovindthakur <ergovindthakur@gmail.com>
2025-05-20 20:33:13 +0530

    Explore basic of for loop

---
+++ 03_Loops/01_Loop.js
@@ -0,0 +1,45 @@
+// Exploring basics of loop in js
+
+/*
+Loop (Iterative statement) -: It is the process in process in programming language to do repeated task using a single statement called loop or iterative statement
+*/
+
+// Printing 3 times "Govind" 
+// Here using three times console.log("Govind");
+console.log("Govind");
+console.log("Govind");
+console.log("Govind");
+
+// Basic loop structure
+/*
+for(start; condition(end); change(iteration,(increment, decrement))){
+  code
+}
+*/
+// printing three times "Govinda" using for loop
+
+for(let i = 1; i<=3; i++){
+     console.log("Govinda"); // here using one line (console.log("Govinda"))
+}
+
+// Note -: Loops play very important role in software application
+// eg -: as we see a list of user profile in a single column (in whatsapp, instagram, web container)
+
+
+// Print 1 to 22
+for(let i = 1; i<=22; i++){
+  console.log(i);
+}
+// Print 22 to 1
+for(let i = 22; i>=1; i--){
+  console.log(i);
+}
+
+// valid loop structure
+
+/*
+for(;;){
+  // Here only semicolon matters in for loop 
+  // it's a valid loop but runs infinite times
+}
+*/
