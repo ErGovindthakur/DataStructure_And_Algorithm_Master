04e11f2adb6256a93612f79944b9aa3d81a38630
ErGovindthakur <ergovindthakur@gmail.com>
2025-05-20 22:18:31 +0530

    optimize factorial code

--- 03_Loops/03_factorial.js
+++ 03_Loops/03_factorial.js
@@ -4,18 +4,25 @@ let findFactorial = (num) => {
      let fact = 1;
      num = Number(num);
 
-     if(isNaN(num)){
-          return "Enter a valid input";
-     }else{
-          if(num<=0){
-               return "Inter a positive number greater than zero";
-          }else{
-               for(let n = num; n>=1; n--){
-                    fact*= n;
-               }
-          }
+     // if(isNaN(num)){
+     //      return "Enter a valid input";
+     // }else{
+     //      if(num<=0){
+     //           return "Inter a positive number greater than zero";
+     //      }else{
+     //           for(let n = num; n>=1; n--){
+     //                fact*= n;
+     //           }
+     //      }
+     // }
+
+
+     // optimized code version
+     if(isNaN(num) || num<=0) return "Enter a valid num must be greater than 0";
+     while(num>0){
+          fact*=num--;
      }
      return `The factorial of Given num = ${fact}`;
 };
 
-console.log(findFactorial(5));
+console.log(findFactorial(7));
