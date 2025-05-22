f9253c8cd4b2f97e518ca5849dc1f2a1f2e21807
ErGovindthakur <ergovindthakur@gmail.com>
2025-05-22 17:44:55 +0530

    solve right angle triangle problem

---
+++ 04_Pattern_Programming/04_RightAngleTriangle.js
@@ -0,0 +1,12 @@
+// print right angle triangle with 5 rows and cols accordingly
+
+let prompt = require("prompt-sync")();
+
+let stars = prompt("Enter the numbers of stars -: ");
+
+for(let rows = 1; rows<=stars; rows++){
+     for(let cols = 1; cols<=rows; cols++){
+          process.stdout.write("* ");
+     }
+     console.log();
+}
