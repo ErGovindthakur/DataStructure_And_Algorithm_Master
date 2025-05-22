c5261b228cb3af1e4e85aeb8049ea8f63f802dd1
ErGovindthakur <ergovindthakur@gmail.com>
2025-05-22 18:05:38 +0530

    solve inverted right angle triangle

---
+++ 04_Pattern_Programming/07_InvertedRightAngleTriangle.js
@@ -0,0 +1,13 @@
+// Printing inverted right angle triangle with 5 rows and cols accordingly
+
+let prompt = require("prompt-sync")();
+
+let stars = prompt("Enter the numbers of stars -: ");
+
+for(let i = stars; i>=1; i--){
+
+     for(let j = i; j>=1; j--){
+         process.stdout.write("* ");
+     }
+     console.log();
+}
