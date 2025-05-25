51a05d1928d49734a6be325d018f700f97708c29
ErGovindthakur <ergovindthakur@gmail.com>
2025-05-26 01:15:49 +0530

    got how to take data from user and store it into an array

---
+++ 05_Array_Basic/02_StoreDataIntoArray.js
@@ -0,0 +1,11 @@
+// Taking data from users and storing it into an array
+
+const prompt = require("prompt-sync")();
+
+// Taking five elements
+let store = [];
+
+for(let i = 0; i<=4; i++){
+     store[i] = Number(prompt("Enter your values -: "));
+};
+console.log(store);
