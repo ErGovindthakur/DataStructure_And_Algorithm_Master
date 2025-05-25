a5c480e0b1fc2e336c280cf40d728532f4020f51
ErGovindthakur <ergovindthakur@gmail.com>
2025-05-26 01:29:45 +0530

    solve sum of n elements of an array problem

---
+++ 05_Array_Basic/03_sumNelementOfArray.js
@@ -0,0 +1,12 @@
+// find the sum of "n" element of an array
+
+let arr = [1,2,3,4,5];
+
+let sum = 0; // it will store the sum
+
+// traversing loop over an array
+for(let i = 0; i<arr.length; i++){
+     sum += arr[i];
+}
+
+console.log(`The sum of an array elements = ${sum}`);
