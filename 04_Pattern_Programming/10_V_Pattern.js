05eddd8ff642b901873e4bfc180d07f2c442f16b
ErGovindthakur <ergovindthakur@gmail.com>
2025-05-25 12:39:37 +0530

    solve V pattern in js

---
+++ 04_Pattern_Programming/10_V_Pattern.js
@@ -0,0 +1,17 @@
+// printing "V" pattern in js
+
+let prompt = require("prompt-sync")();
+
+let rows = Number(prompt("Enter the number of rows -: "));
+let cols = Number(prompt("Enter the number of cols -: "));
+
+for(let i = 1; i<=rows; i++){
+     for(let j=1; j<=cols; j++){
+          // if (i==j || i+j==cols-1)
+          if (i==j || i+j==cols){
+               process.stdout.write("*");
+          }
+          process.stdout.write(" ");
+     }
+     console.log();
+}
