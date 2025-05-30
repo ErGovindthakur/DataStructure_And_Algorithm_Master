f15e8a70d3b108eb52b70f61941c2f5e2af0c8ff
ErGovindthakur <ergovindthakur@gmail.com>
2025-05-30 11:56:35 +0530

    optimize the time complexity of this code O(n^2) to O(n)

--- 06_Advance_Array/03_LeftRotationByKElem.js
+++ 06_Advance_Array/03_LeftRotationByKElem.js
@@ -13,4 +13,23 @@ let leftRotationByKElem1 = (arr, k) => {
   return arr;
 };
 
-console.log(leftRotationByKElem1([1, 2, 3, 4, 5], 6));
+// console.log(leftRotationByKElem1([1, 2, 3, 4, 5], 6));
+
+let arr = [1,2,3,4,5];
+let k = 2;
+let leftRotationByKElem2 = (i,j) => {
+
+  while (i < j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    i++;
+    j--;
+  }
+
+};
+leftRotationByKElem2(0,k-1);
+leftRotationByKElem2(k,arr.length-1);
+leftRotationByKElem2(0,arr.length-1);
+
+console.log(arr);
