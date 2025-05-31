296e05f7bbdf10912a98d71b2bc64813b7114c25
ErGovindthakur <ergovindthakur@gmail.com>
2025-05-31 22:29:28 +0530

    reviews and modified codes

--- 04_Pattern_Programming/08_MirrorRightAngleTriangle.js
+++ 04_Pattern_Programming/08_MirrorRightAngleTriangle.js
@@ -4,12 +4,14 @@ let prompt = require("prompt-sync")();
 
 let space = prompt("Enter the numbers of stars -: ");
 
-// Inverted right angle triangle
 for(let i = 1; i<=space; i++){
-
+     
+     // Inverted right angle triangle
      for(let j = 1; j<=space-i; j++){
           process.stdout.write(" ");
      }
+
+     // Simple right angle triangle
      for(let k = 1; k<=i; k++){
           process.stdout.write("*");
      }
