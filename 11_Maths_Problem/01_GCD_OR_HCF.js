06f68415c5459b09d0b6e748ac86d63b8c69f45e
ErGovindthakur <ergovindthakur@gmail.com>
2025-06-16 13:58:23 +0530

    gcd problem in optimized way using euclidean algorithm with iterative way

--- 11_Maths_Problem/01_GCD_OR_HCF.js
+++ 11_Maths_Problem/01_GCD_OR_HCF.js
@@ -1,18 +1,29 @@
 // Exploring "Greatest common Divisor" or "Highest common Factor"
 
-
 // brute force approach to solve GCD or HCF problem
 
-let findGcdOrHcf = (num1,num2) => {
+let findGcdOrHcf = (num1, num2) => {
+  let max = Math.min(num1, num2);
 
-     let max = Math.min(num1, num2);
-     
-     for(let i = max; i>=1; i--){
-          if((num1%i === 0) && (num2%i===0)){
-               return i;
-          }
-          // console.log(i)
-     }
+  for (let i = max; i >= 1; i--) {
+    if (num1 % i === 0 && num2 % i === 0) {
+      return i;
+    }
+    // console.log(i)
+  }
+};
+// console.log(findGcdOrHcf(24,32));
 
+// Solving the same problem with optimized way via "Euclidean Algorithm"
+
+// 1. Iterative Approach
+let gcd = (a,b) => {
+    while(b!==0){
+        let temp = b; // 32, 20, 12, 8, 4
+        b = a % b;    // 20, 12, 8,  4, 0
+        a = temp;     // 32, 20, 12, 8, 
+    }
+    return a;
 }
-console.log(findGcdOrHcf(24,32));
+
+console.log(gcd(20,32));
