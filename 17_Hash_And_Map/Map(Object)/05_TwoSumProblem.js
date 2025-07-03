9d45d2e8d91d6501ee0133f0a02eb762abf2a70d
ErGovindthakur <ergovindthakur@gmail.com>
2025-07-03 18:37:37 +0530

    solve two sum problem using optimized way

--- 17_Hash_And_Map/Map(Object)/05_TwoSumProblem.js
+++ 17_Hash_And_Map/Map(Object)/05_TwoSumProblem.js
@@ -1,13 +1,27 @@
 // Solve two sum problem using brute force approach tmc O(n^2)
 
-let twoSum = (arr,target) => {
-     for(let i = 0; i<arr.length; i++){
-          for(let j = i+1; j<arr.length; j++){
-               if(arr[i]+arr[j]===target){
-                    return [i,j]
-               }
-          }
-     }
-     return -1;
-}
-console.log(twoSum([3,4,5,2,1],6))
+let twoSum = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return -1;
+};
+console.log(twoSum([3, 4, 5, 2, 1], 6));
+
+// Optimize way to find two sum problem
+
+let twoSum2 = (arr, target) => {
+  let tgMap = new Map();
+  for (let i = 0; i < arr.length; i++) {
+    if (tgMap.has(target - arr[i])) {
+      return [i, tgMap.get(target - arr[i])];
+    } else {
+      tgMap.set(arr[i], i);
+    }
+  }
+  return [-1, -1];
+};
