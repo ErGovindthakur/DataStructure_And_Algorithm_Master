da86c1978e5a347fa7127898194c8da084a3a44e
ErGovindthakur <ergovindthakur@gmail.com>
2025-07-04 11:41:21 +0530

    solve the same problem using set and reduce tmc O(n^2) to O(n+m+k) times

--- 17_Hash_And_Map/Hash(Array)/04_IntersectionOfTwoArray.js
+++ 17_Hash_And_Map/Hash(Array)/04_IntersectionOfTwoArray.js
@@ -13,6 +13,25 @@ let findIntersection = (arr1, arr2) => {
   return outputArr;
 };
 
-console.log(findIntersection([1,2,2,1,3],[1,3,2,1]));
+// console.log(findIntersection([1,2,2,1,3],[1,3,2,1]));
 
 // Time complexity => O(n^2)
+
+// Optimized solution using map
+
+let findIntersection2 = (arr1,arr2) => {
+     let ans = [];
+     // put the arr1 value into the map
+     let dupCheckMap = new Set(arr1);
+
+     // iterate over 2nd arr
+     for(let i = 0; i<arr2.length; i++){
+          if(dupCheckMap.has(arr2[i]) && !ans.includes(arr2[i])){
+               ans.push(arr2[i]);
+          }
+     }
+return ans
+}
+console.log(findIntersection2([1,2,2,3],[1,2,4,3,1]))
+
+//O(n + m * k)
