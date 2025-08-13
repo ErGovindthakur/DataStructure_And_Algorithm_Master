730be0da5ea91b710bce1ba56e1c0600b88d68ab
ErGovindthakur <ergovindthakur@gmail.com>
2025-08-13 22:47:00 +0530

    explore abstraction in js in depth

---
+++ 18_Js_Interview_Adv/OOP_In_Js/04_Abstraction.js
@@ -0,0 +1,27 @@
+// Exploring Abstraction in js
+
+// Abstraction => It means hiding the complex internal process from users which are actually not necessary for them , but providing a simplified interface to the the task easily, weather that task quite difficult behind the scene
+
+
+class coffeeMachine {
+
+     start(){
+          this.#heatWater();
+          this.#brewCoffee();
+          console.log("coffee done !")
+     }
+
+     #heatWater(){
+          console.log("Water is heating...")
+     }
+
+     #brewCoffee(){
+          console.log("Brewing the coffee...")
+     }
+}
+
+// creating instance 
+
+const coffee = new coffeeMachine();
+
+coffee.start();
