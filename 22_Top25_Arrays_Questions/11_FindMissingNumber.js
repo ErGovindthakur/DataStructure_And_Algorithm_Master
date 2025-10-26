8413d088eb0f49bdd21f0e702fb436e629dcd154
ErGovindthakur <ergovindthakur@gmail.com>
2025-10-26 23:16:06 +0530

    write more readable code

--- 22_Top25_Arrays_Questions/11_FindMissingNumber.js
+++ 22_Top25_Arrays_Questions/11_FindMissingNumber.js
@@ -4,16 +4,19 @@ let findMissingNumber = (arr) => {
   if (n == 0) return 0;
   if (n == 1) return arr[0] == 0 ? 1 : 0;
 
-  let actualSum = 0;
-  let arrSum = 0;
+//   let actualSum = 0;
+//   let arrSum = 0;
 
-  arrSum = arr.reduce((acc, val) => acc + val, 0);
-  console.log(arrSum);
+ let  arrSum = arr.reduce((acc, val) => acc + val, 0);
+  //   console.log(arrSum);
 
-  for (let i = 0; i <= n; i++) {
-    actualSum += i;
-  }
-  console.log(actualSum);
+  //   for (let i = 0; i <= n; i++) {
+  //     actualSum += i;
+  //   }
+  //   console.log(actualSum);
+
+  // formula based sum
+  let actualSum = Math.floor((n * (n + 1)) / 2);
 
   return actualSum - arrSum;
 };
