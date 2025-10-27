00ee5d39300e24bedd8b63226e0317e0ef8fa92f
ErGovindthakur <ergovindthakur@gmail.com>
2025-10-27 10:16:57 +0530

    solve two sum problem using hashMap

--- 22_Top25_Arrays_Questions/13_2Sum.js
+++ 22_Top25_Arrays_Questions/13_2Sum.js
@@ -13,4 +13,27 @@ let TwoSum = (arr) => {
      }
      return [-1,-1];
 }
-console.log(TwoSum([2,7,11,15]));
+// TMC => O(n^2), SPC => O(n)
+// console.log(TwoSum([2,7,11,15]));
+
+// let mp = new Map();
+// mp.set(1,3);
+// mp.set(2,5);
+
+// console.log(mp.has(2));
+
+let TwoSum2 = (arr) => {
+     if(arr.length<2)return arr;
+     let map = new Map();
+     let target = 9;
+     for(let i = 0; i<arr.length; i++){
+          let diff = target - arr[i];
+          if(map.has(diff)){
+               return [map.get(diff),i];
+          }
+          map.set(arr[i],i);
+     }
+     return -1;
+}
+// TMC => O(n), SPC => O(n);
+console.log(TwoSum2([2,7,11,15]));
