fe46d5ef1abf94ed94e32189949d7373335657b4
ErGovindthakur <ergovindthakur@gmail.com>
2025-11-01 22:01:31 +0530

    solve the container with most water problem using two pointer approach

--- 22_Top25_Arrays_Questions/22_ContainerWithMostWater.js
+++ 22_Top25_Arrays_Questions/22_ContainerWithMostWater.js
@@ -17,4 +17,31 @@ let containerWithMostWater = (height) => {
   return maxWaterArea;
 };
 
-console.log(containerWithMostWater([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+// console.log(containerWithMostWater([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+
+let containerWithMostWaterTwoPointer = (height) => {
+  if (height.length === 0) return 0;
+
+  let stLine = 0; // starting height
+  let endLine = height.length - 1; // ending height
+
+  let maxArea = 0; // stores max water
+
+  while (stLine < endLine) {
+    let width = endLine - stLine;
+    let hgt = Math.min(height[stLine], height[endLine]);
+    let area = width * hgt;
+
+    maxArea = Math.max(area, maxArea);
+
+    // actual task
+    if (height[stLine] < height[endLine]) {
+      stLine++;
+    } else {
+      endLine--;
+    }
+  }
+  return maxArea;
+};
+
+console.log(containerWithMostWaterTwoPointer([1, 8, 6, 2, 5, 4, 8, 3, 7]));
