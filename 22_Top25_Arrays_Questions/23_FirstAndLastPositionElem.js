9ef2cd27a55fdc3112aa42700de2dc575bc22958
ErGovindthakur <ergovindthakur@gmail.com>
2025-11-01 22:34:48 +0530

    fixes some minor bugs

--- 22_Top25_Arrays_Questions/23_FirstAndLastPositionElem.js
+++ 22_Top25_Arrays_Questions/23_FirstAndLastPositionElem.js
@@ -1,18 +1,33 @@
 let firstAndLastIdxOfTarget = (arr,target) => {
 
      if(arr.length === 0) return [-1,-1];
-     let result = new Set();
+     let first = -1;
+     let last = -1;
+
      for(let i = 0; i<arr.length; i++){
           if(arr[i]===target){
-               let lastIdx = i;
-               result.add(i,lastIdx);
+               if(first === -1) first = i;
+               last = i;
           }
      }
-     if(arr.includes(target)){
-          return Array.from(result);
-     }
-     return [-1,-1];
+     return [first,last];
 }
 console.log(firstAndLastIdxOfTarget([5,7,7,8,8,10],8));
 console.log(firstAndLastIdxOfTarget([5,7,7,8,8,10],6));
 console.log(firstAndLastIdxOfTarget([],0));
+
+// let firstAndLastIdxOfTarget2 = (arr,target) => {
+//      if(arr.length===0) return [-1,-1];
+
+//      let st = 0;
+//      let end = arr.length - 1;
+
+//      while(st<end){
+//           let mid = Math.floor((st+end)/2);
+//           console.log(arr[mid]);
+//           st++;
+//           end--;
+
+//      }
+// }
+// console.log(firstAndLastIdxOfTarget2([5,7,7,8,8,10],8));
