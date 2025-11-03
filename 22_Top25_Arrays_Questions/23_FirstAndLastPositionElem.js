3d6ec937f59bc067713222c708db4637e0200033
ErGovindthakur <ergovindthakur@gmail.com>
2025-11-03 21:45:17 +0530

    solve first and last occurence index of target in sorted array

--- 22_Top25_Arrays_Questions/23_FirstAndLastPositionElem.js
+++ 22_Top25_Arrays_Questions/23_FirstAndLastPositionElem.js
@@ -12,22 +12,39 @@ let firstAndLastIdxOfTarget = (arr,target) => {
      }
      return [first,last];
 }
-console.log(firstAndLastIdxOfTarget([5,7,7,8,8,10],8));
-console.log(firstAndLastIdxOfTarget([5,7,7,8,8,10],6));
-console.log(firstAndLastIdxOfTarget([],0));
+// console.log(firstAndLastIdxOfTarget([5,7,7,8,8,10],8));
+// console.log(firstAndLastIdxOfTarget([5,7,7,8,8,10],6));
+// console.log(firstAndLastIdxOfTarget([],0));
 
-// let firstAndLastIdxOfTarget2 = (arr,target) => {
-//      if(arr.length===0) return [-1,-1];
+let searchFirstAndLastIndex = (arr, target) => {
+  const findIndex = (findFirst) => {
+    let low = 0, high = arr.length - 1;
+    let ans = -1;
 
-//      let st = 0;
-//      let end = arr.length - 1;
+    while (low <= high) {
+      let mid = Math.floor((low + high) / 2);
 
-//      while(st<end){
-//           let mid = Math.floor((st+end)/2);
-//           console.log(arr[mid]);
-//           st++;
-//           end--;
+      if (arr[mid] === target) {
+        ans = mid;
+        if (findFirst) {
+          high = mid - 1; // keep searching left side
+        } else {
+          low = mid + 1; // keep searching right side
+        }
+      } else if (arr[mid] < target) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
 
-//      }
-// }
-// console.log(firstAndLastIdxOfTarget2([5,7,7,8,8,10],8));
+    return ans;
+  };
+
+  return [findIndex(true), findIndex(false)];
+};
+
+// Example runs
+console.log(searchRange([5,7,7,8,8,10], 8)); // [3,4]
+console.log(searchRange([5,7,7,8,8,10], 6)); // [-1,-1]
+console.log(searchRange([], 0));             // [-1,-1]
