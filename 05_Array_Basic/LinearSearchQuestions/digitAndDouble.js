e38521605682ac5a3a62f0cc464e51f3bb86c71d
ErGovindthakur <ergovindthakur@gmail.com>
2026-06-11 01:07:19 +0530

    explore essentials of obj in js

--- 05_Array_Basic/LinearSearchQuestions/digitAndDouble.js
+++ 05_Array_Basic/LinearSearchQuestions/digitAndDouble.js
@@ -16,3 +16,20 @@ let digitAndDouble = (nums) => {
 
 console.log(digitAndDouble([10,2,5,3]))
 console.log(digitAndDouble([3,1,7,11]))
+
+/*
+Note : Most Important DSA Pattern
+
+Whenever you hear:
+
+Find duplicates
+Find frequency
+Check existence
+Two Sum
+Contains Duplicate
+Anagram
+Intersection
+
+Think about Object, Map, Set
+And in Obj all CRUD has O(1) time complexity
+*/
