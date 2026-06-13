f8495a8d95d6e120ffd090f60cd55794d51acd64
ErGovindthakur <ergovindthakur@gmail.com>
2026-06-13 16:37:34 +0530

    solve merge two sorted array problem using optimal approach

--- 05_Array_Basic/LinearSearchQuestions/MergeSortedArr.js
+++ 05_Array_Basic/LinearSearchQuestions/MergeSortedArr.js
@@ -32,6 +32,37 @@ let mergeSortedArr = (arr1, arr2) => {
   }
   return mergedArr;
 };
-console.log(mergeSortedArr([1, 2, 3, 0, 0, 0], [2, 5, 6]));
-console.log(mergeSortedArr([1],[0]));
-console.log(mergeSortedArr([1],[]))
+// console.log(mergeSortedArr([1, 2, 3, 0, 0, 0], [2, 5, 6]));
+// console.log(mergeSortedArr([1],[0]));
+// console.log(mergeSortedArr([1],[]));
+
+// Merging sorted Array In-place means without using extra spaces
+
+let merge = (arr1, m, arr2, n) => {
+  // since we are starting merging elem from rear
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+
+  while (i >= 0 && j >= 0) {
+    if (arr1[i] > arr2[j]) {
+      arr1[k] = arr1[i];
+      k--;
+      i--;
+    } else {
+      arr1[k] = arr2[j];
+      k--;
+      j--;
+    }
+  }
+  while (i >= 0) {
+    arr1[k--] = arr1[i--];
+  }
+  while (j >= 0) {
+    arr1[k--] = arr2[j--];
+  }
+  return arr1;
+};
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
+console.log(merge([1], 1, [0], 0));
+console.log(merge([1], 1, [], 0));
