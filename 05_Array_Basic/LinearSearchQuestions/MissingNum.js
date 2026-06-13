5a24f21f42a5a790e2e05abdc31580a68ce467da
ErGovindthakur <ergovindthakur@gmail.com>
2026-06-14 02:26:33 +0530

    solve same problem in more simpler way

--- 05_Array_Basic/LinearSearchQuestions/MissingNum.js
+++ 05_Array_Basic/LinearSearchQuestions/MissingNum.js
@@ -1,15 +1,7 @@
 let findMissingNum = (arr) => {
      let n = arr.length;
 
-     let maxNum = -Infinity;
-
-     for(let i = 0; i<n; i++){
-          if(maxNum<arr[i]){
-               maxNum = arr[i]; // found max num
-          }
-     }
-
-     let expectedSum = Math.floor((maxNum * (maxNum+1))/2);
+     let expectedSum = Math.floor((n * (n+1))/2);
 
      let actualSum = arr.reduce((acc,val)=>acc+val,0);
 
