77c9b1fc13130272cf536f5d9e3199aef5841a6f
ErGovindthakur <ergovindthakur@gmail.com>
2026-06-28 11:15:12 +0530

    explore binary search

--- 12_BinarySearchAlogorithm/01_BinarySearch.js
+++ 12_BinarySearchAlogorithm/01_BinarySearch.js
@@ -18,6 +18,31 @@ let binarySearch = (arr,target) => {
      return "Target not found";
 }
 
-console.log(binarySearch([1,2,3,4,5,6,7,8,9,10,11,12,13],11));
+// console.log(binarySearch([1,2,3,4,5,6,7,8,9,10,11,12,13],11));
 
 // TMC => O(long) and SPC => O(1)
+
+let binarySearchAgain =  (arr,target) => {
+     let n = arr.length;
+     // if(n<2 && arr[0]===target)return true;
+
+     let st = 0;
+     let end = n-1;
+
+     while(st<=end){
+
+          let mid = Math.floor((st+end)/2);
+          console.log(mid)
+          if(arr[mid]===target){
+               return true;
+          }
+          if(arr[mid]<target){
+               st = mid + 1;
+          }
+          if(arr[mid]>target){
+               end = mid-1;
+          }
+     }
+     return false;
+}
+console.log(binarySearchAgain([1,2,3,4,5,6,7,8,9,10,11,12,13],12))
